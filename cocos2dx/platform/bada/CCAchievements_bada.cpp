c8bfcf73159f78371478241618b898a6e67f8420
Nick Flink <nickflink@github.com>
2012-09-02 18:13:43 +0200

    remove content for not yet supported platforms

--- cocos2dx/platform/bada/CCAchievements_bada.cpp
+++ cocos2dx/platform/bada/CCAchievements_bada.cpp
@@ -1,5 +1,6 @@
 /****************************************************************************
-Copyright (c) 2010 cocos2d-x.org
+Copyright (c) 2012 cocos2d-x.org
+Copyright (c) 2012 Nick Flink
 
 http://www.cocos2d-x.org
 
@@ -20,136 +21,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
+
+THIS PLATFORM IS NOT YET SUPPORTED!
 ****************************************************************************/
 
-#include "CCAchievements_bada.h"
-#include "ccMacros.h"
-#include "CCEGLView_bada.h"
-
-using namespace Osp::Ui;
-using namespace Osp::Uix;
-
-NS_CC_BEGIN;
-
-
-CCAchievements::CCAchievements()
-: m_pAccelDelegate(NULL)
-, m_pSensor(NULL)
-, m_bEnabled(false)
-{
-
-}
-
-CCAchievements::~CCAchievements() 
-{
-	CC_SAFE_DELETE(m_pSensor);
-}
-
-CCAchievements* CCAchievements::sharedAchievements() 
-{
-	static CCAchievements s_CCAchievements;
-	return &s_CCAchievements;
-}
-
-void CCAchievements::setDelegate(CCAchievementsDelegate* pDelegate)
-{
-	m_pAccelDelegate = pDelegate;
-	if (pDelegate != NULL)
-	{
-		setEnable(true);
-	}
-	else
-	{
-		setEnable(false);
-	}
-}
-
-void CCAchievements::OnDataReceived(SensorType sensorType, SensorData& sensorData, result r)
-{
-	long timeStamp = 0;
-	float x = 0.0, y = 0.0, z = 0.0;
-
-	sensorData.GetValue((SensorDataKey)ACCELERATION_DATA_KEY_TIMESTAMP, timeStamp);
-	sensorData.GetValue((SensorDataKey)ACCELERATION_DATA_KEY_X, x);
-	sensorData.GetValue((SensorDataKey)ACCELERATION_DATA_KEY_Y, y);
-	sensorData.GetValue((SensorDataKey)ACCELERATION_DATA_KEY_Z, z);
-
-	/*
-	 * Because the axes are not swapped when the device's screen orientation changes.
-	 * So we should swap it here.
-	 */
-	Orientation orientation = CCEGLView::sharedOpenGLView().GetOrientation();
-	if (orientation == ORIENTATION_LANDSCAPE || orientation == ORIENTATION_LANDSCAPE_REVERSE)
-	{
-		float tmp = x;
-		x = -y;
-		y = tmp;
-	}
-
-	CCAcceleration AccValue;
-	AccValue.x = -x;
-	AccValue.y = -y;
-	AccValue.z = -z;
-	AccValue.timestamp = timeStamp;
-
-	if (m_pAccelDelegate != NULL)
-	{
-		m_pAccelDelegate->didAccelerate(&AccValue);
-	}
-	//AppLog("##TimeStamp:[%d], Accel.x,y,z:[%f,%f,%f]", timeStamp, x, y, z);
-}
-
-void CCAchievements::setEnable(bool bEnable)
-{
-	result	r = E_INVALID_STATE;
-	if (m_bEnabled == bEnable)
-	{
-		return;
-	}
-
-	m_bEnabled = bEnable;
-
-	if (m_bEnabled)
-	{
-		bool available = false;
-		long interval = 50;
-
-		CC_SAFE_DELETE(m_pSensor);
-		m_pSensor = new SensorManager();
-		m_pSensor->Construct();
-
-		available = m_pSensor->IsAvailable(SENSOR_TYPE_ACCELERATION);
-		if (available)
-		{
-			long intervalTemp = 0;
-			m_pSensor->GetMaxInterval(SENSOR_TYPE_ACCELERATION, intervalTemp);
-			if (interval > intervalTemp)
-			{
-				interval = intervalTemp;
-			}
-			m_pSensor->GetMinInterval(SENSOR_TYPE_ACCELERATION, intervalTemp);
-			if (interval < intervalTemp)
-			{
-				interval = intervalTemp;
-			}
-			r = m_pSensor->AddSensorListener(*this, SENSOR_TYPE_ACCELERATION, interval, true);
-		}
-		else
-		{
-			CCLOG("Achievements Sensor unavailable!");
-			delete m_pSensor;
-			m_pSensor = NULL;
-		}
-	}
-	else
-	{
-		if (m_pSensor != NULL)
-		{
-			r = m_pSensor->RemoveSensorListener(*this);
-			delete m_pSensor;
-			m_pSensor = NULL;
-		}
-	}
-}
-
-NS_CC_END;
--- cocos2dx/platform/marmalade/CCAchievements_marmalade.cpp
+++ cocos2dx/platform/marmalade/CCAchievements_marmalade.cpp
@@ -1,87 +1,26 @@
 /****************************************************************************
- Copyright (c) 2011      cocos2d-x.org   http://cocos2d-x.org
- Copyright (c) 2011      Максим Аксенов
- Copyright (c) 2011      Giovanni Zito, Francis Styck
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
+Copyright (c) 2012 cocos2d-x.org
+Copyright (c) 2012 Nick Flink
 
-#include "CCAchievements_marmalade.h"
-#include "ccMacros.h"
+http://www.cocos2d-x.org
 
-namespace cocos2d
-{
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-CCAchievements* CCAchievements::m_spCCAchievements = NULL;
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
 
-CCAchievements::CCAchievements() : m_pAccelDelegate(NULL)
-{
-}
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 
-CCAchievements::~CCAchievements() 
-{
-	if( m_spCCAchievements ) {
-		delete m_spCCAchievements ;
-		m_spCCAchievements = NULL;
-	}
-}
-
-CCAchievements* CCAchievements::sharedAchievements() 
-{
-
-	if (m_spCCAchievements == NULL)
-	{
-		m_spCCAchievements = new CCAchievements();
-	}
-
-	return m_spCCAchievements;
-}
-
-void CCAchievements::setDelegate(CCAchievementsDelegate* pDelegate) 
-{
-	m_pAccelDelegate = pDelegate;
-
-	if (pDelegate)
-	{		
-		if (s3eAchievementsStart() != S3E_RESULT_SUCCESS)
-		{
-			CCLog("s3eAchievementsStart() - ERROR\n");
-		}
-	}
-	else
-	{
-		s3eAchievementsStop();
-	}
-}
-
-void CCAchievements::update(float x, float y, float z, uint64 sensorTimeStamp) 
-{
-	if (m_pAccelDelegate)
-	{
-		m_obAccelerationValue.x = ((double)x)/S3E_ACCELEROMETER_1G ;
-		m_obAccelerationValue.y = ((double)y)/S3E_ACCELEROMETER_1G ;
-		m_obAccelerationValue.z = ((double)z)/S3E_ACCELEROMETER_1G ;
-		m_obAccelerationValue.timestamp = (double)(sensorTimeStamp / 1000.0);
-
-		m_pAccelDelegate->didAccelerate(&m_obAccelerationValue);
-	}	
-}
-
-} // end of namespace cococs2d
-
+THIS PLATFORM IS NOT YET SUPPORTED!
+****************************************************************************/

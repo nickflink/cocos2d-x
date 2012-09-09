be0771156428a48a56cfc79305018a4d8928a771
Nick Flink <nickflink@github.com>
2012-09-09 11:56:55 +0200

    setting up leaderboard structure

---
+++ cocos2dx/platform/ios/LeaderboardDelegateWrapper.h
@@ -0,0 +1,74 @@
+/****************************************************************************
+ Copyright (c) 2012 cocos2d-x.org
+ Copyright (c) 2012 Nick Flink
+ 
+ http://www.cocos4d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+#import "CCAchievementsDelegate.h"
+#import <Foundation/Foundation.h>
+#import <GameKit/GameKit.h>
+
+/*@protocol AchievementsDelegateProtocol
+
+-(void) onLocalPlayerAuthenticationChanged;
+-(void) onAchievementReported:(GKAchievement*)achievement;
+-(void) onAchievementsLoaded:(NSDictionary*)achievements;
+-(void) onResetAchievements:(bool)success;
+-(void) onAchievementsViewDismissed;
+
+@end*/
+
+
+@interface AchievementsDispatcher : NSObject<GKAchievementViewControllerDelegate/*, AchievementsDelegateProtocol*/>
+{
+	//id<AchievementsDelegateProtocol> delegate;
+	//cocos2d::CCAchievementsDelegate *delegate_;
+	//id<GameKitHelperProtocol> delegate;
+	bool isGameCenterAvailable;
+	NSError* lastError;
+	
+	NSMutableDictionary* achievements;
+	NSMutableDictionary* cachedAchievements;
+}
+
+//@property (nonatomic, retain) id<GameKitHelperProtocol> delegate;
+@property (readwrite) cocos2d::CCAchievementsDelegate *delegate_;
+@property (nonatomic, readonly) bool isGameCenterAvailable;
+@property (nonatomic, readonly) NSError* lastError;
+@property (nonatomic, readonly) NSMutableDictionary* achievements;
+
++ (id) sharedAchievementsDispather;
+- (id) init;
+- (void) addDelegate: (cocos2d::CCAchievementsDelegate *) delegate;
+- (void) achievementViewControllerDidFinish:(GKAchievementViewController *)viewController;
+- (void) onLocalPlayerAuthenticationChanged;
+- (void) resetAchievements;
+- (void) saveCachedAchievements;
+- (void) reportCachedAchievements;
+
+/*- (void) onAchievementReported:(GKAchievement*)achievement;
+- (void) onAchievementsLoaded:(NSDictionary*)achievements;
+- (void) onResetAchievements:(bool)success;
+- (void) onAchievementsViewDismissed;*/
+
+
+@end

846202bbcdf0944ff77cf0ca404d9140a24d3fac
Nick Flink <nickflink@github.com>
2014-01-02 15:59:31 +0100

    add applicationDidReceiveMemoryWarning to the ApplicationProtocol

--- cocos/2d/platform/CCApplicationProtocol.h
+++ cocos/2d/platform/CCApplicationProtocol.h
@@ -56,6 +56,13 @@ public:
     * @lua NA
     */
     virtual void applicationWillEnterForeground() = 0;
+  
+    /**
+    @brief  This function will be called when the application receives a memory warning.
+    * @js NA
+    * @lua NA
+    */
+    virtual void applicationDidReceiveMemoryWarning() = 0;
 
     /**
     @brief    Callback by Director for limit FPS.
